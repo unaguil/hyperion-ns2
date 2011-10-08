dd070f782dd19f6d261d88384527713d23eeaf6e
Unai Aguilera <gkalgan@gmail.com>
2011-10-08 08:47:06 +0000

    * Measures updated according new classes distribution.

--- experiments/measures/generic/ReceivedMessages.py
+++ experiments/measures/generic/ReceivedMessages.py
@@ -7,7 +7,7 @@ class ReceivedMessages(GenericMeasure):
 	"""Due to broadcast issues received package number can be greater than SentMessages""" 
 	
 	def __init__(self, period, simulationTime):
-		GenericMeasure.__init__(self, r"DEBUG peer.PeerAgentJ  - Peer [0-9]+ received .*? ([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
+		GenericMeasure.__init__(self, r"DEBUG peer.BasicPeer  - Peer [0-9]+ received .*? ([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
 	
 	def parseLine(self, line):
 		self.parseInc(line)
--- experiments/measures/generic/SentMessages.py
+++ experiments/measures/generic/SentMessages.py
@@ -6,7 +6,7 @@ class SentMessages(GenericMeasure):
 	""" Total number of sent messages of any type """
 	
 	def __init__(self, period, simulationTime):
-		GenericMeasure.__init__(self, r"DEBUG peer.PeerAgentJ  - Peer [0-9]+ sending.*?([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
+		GenericMeasure.__init__(self, r"DEBUG peer.BasicPeer  - Peer [0-9]+ sending.*?([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
 			
 	def parseLine(self, line):
 		self.parseInc(line)
