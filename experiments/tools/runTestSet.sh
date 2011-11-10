5d73bd80c37d75be91e573e0f3bffcd3bb084f8f
Unai Aguilera <gkalgan@gmail.com>
2011-11-10 08:10:23 +0000

    * Experiment results are now saved to a directory inside the passed outputdir.  * Testset start timestamp is appended to directory name.

--- experiments/tools/runTestSet.sh
+++ experiments/tools/runTestSet.sh
@@ -6,11 +6,14 @@ then
   	exit $E_BADARGS
 fi
 
+testname=$(basename $1)-`date '+%Y-%m-%d-%H-%M-%S'`
+
 mkdir $2
+mkdir $2/$testname
 
 for FILE in $(ls $1)
 do
-	command="python Experiment.py -c $1 -f $FILE -o $2/output-$FILE.txt"
+	command="python Experiment.py -c $1 -f $FILE -o $2/$testname/output-$FILE.txt"
 	echo "Running $command"
 	$command
 done
