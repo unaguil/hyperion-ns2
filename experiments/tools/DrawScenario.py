beba6916c03fd251c4795febe79f2aeb897626b8
Unai Aguilera <gkalgan@gmail.com>
2011-11-13 18:46:26 +0000

    * Topology draw fixed.

--- experiments/tools/DrawScenario.py
+++ experiments/tools/DrawScenario.py
@@ -57,7 +57,8 @@ def __parseScenario(scenarioFilePath):
 def __getDistance(nodeA, nodeB):
     x1, y1, z1 = nodeA.getPos()
     x2, y2, z2 = nodeB.getPos()
-    return math.sqrt((x1 - y2)**2 + (y1 - y2)**2 + (z1 - z1)**2)
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    return distance 
 
 def __getNeighbours(nodes):
     neighbourTable = {}
@@ -66,8 +67,14 @@ def __getNeighbours(nodes):
         neighbourTable[node] = []
         for otherNode in nodes:
             if otherNode is not node:
-                if __getDistance(node, otherNode) <= TRANSMISSION_RANGE:
-                    neighbourTable[node].append(otherNode)
+                distance = __getDistance(node, otherNode)
+                #print node.getPos()
+                #print otherNode.getPos()
+                #print 'Distance from %s to %s : %f' % (node.getID(), otherNode.getID(), distance)
+                if distance <= TRANSMISSION_RANGE:
+                    #check inverse relation
+                    if not otherNode in neighbourTable or not node in neighbourTable[otherNode]:
+                        neighbourTable[node].append(otherNode)
                     
     return neighbourTable
 
@@ -95,10 +102,11 @@ def __drawScenario(scenarioFilePath, outputFile):
         _y1 = y1/ymax
         
         for neighbour in neighbours:
-            x2, y2, z2 = node.getPos()
+            x2, y2, z2 = neighbour.getPos()
             _x2 = x2/xmax
             _y2 = y2/ymax
-            print 'Line from %f,%f to %f,%f' % (_x1, y1, _x2, _y2)
+            #print 'Line from %s to %s' % (node.getID(), neighbour.getID())
+            ctx.move_to(_x1, _y1)
             ctx.line_to(_x2, _y2)
             ctx.stroke()
     
@@ -106,17 +114,18 @@ def __drawScenario(scenarioFilePath, outputFile):
         x, y, z = node.getPos()
         _x = x/xmax
         _y = y/ymax
-        print '%s X: %f %f' %(node.getID(), _x, _y)
+        #print '%s X: %f %f' %(node.getID(), _x, _y)
         ctx.set_source_rgb(0, 0, 0)
         ctx.set_line_width(0.002)   
         ctx.arc(_x, _y, .01, 0, 2*math.pi)
-        ctx.stroke()
+        ctx.fill()
         
         #draw node id
-        ctx.select_font_face("Georgia", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        ctx.select_font_face("Georgia")
         ctx.set_font_size(.01)
         x_bearing, y_bearing, width, height = ctx.text_extents(node.getID())[:4]
         ctx.move_to(_x - width / 2 - x_bearing, _y - height / 2 - y_bearing)
+        ctx.set_source_rgb(1, 1, 1)
         ctx.show_text(node.getID())
         ctx.stroke() 
         
