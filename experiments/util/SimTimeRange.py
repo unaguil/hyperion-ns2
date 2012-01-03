d64f16a5d0b0a48bd41e3f9ba42386ec44279bd4
Unai Aguilera <gkalgan@gmail.com>
2012-01-03 11:13:59 +0000

    * Time range modified to be (INIT, DURATION) instead of (INIT, END).

--- experiments/util/SimTimeRange.py
+++ experiments/util/SimTimeRange.py
@@ -1,11 +1,13 @@
 
 def getTimeRange(timeRange, finishTime, discardTime):
-        init, end = timeRange
+        init, duration = timeRange
         
         if init == 'START':
             init = discardTime
             
-        if end == 'END' or end >= finishTime:
+        end = init + duration
+            
+        if end >= finishTime:
             end = finishTime
             
         return (init, end)
