96d5ddbdea64b2be52bb55500053551c9a54e594
Unai Aguilera <gkalgan@gmail.com>
2012-01-23 15:10:21 +0000

    * Regular expression updated.

--- experiments/measures/multicast/StartedSearches.py
+++ experiments/measures/multicast/StartedSearches.py
@@ -5,7 +5,7 @@ class StartedSearches(GenericMeasure):
 	"""Total number of started searches"""
 	
 	def __init__(self, period, simulationTime):
-		GenericMeasure.__init__(self, r'DEBUG multicast.search.ParameterSearchImpl  - Peer [0-9]+ started search for parameters \[.*?\].*?([0-9]+\,[0-9]+).*?', period, simulationTime, Units.MESSAGES)
+		GenericMeasure.__init__(self, r'DEBUG .*?  - Peer [0-9]+ started search for parameters .*?', period, simulationTime, Units.MESSAGES)
 			
 	def parseLine(self, line):
 		self.parseInc(line)
