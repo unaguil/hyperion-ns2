c0ba5ffed96a29373697a952b45dcfa49b449a98
Unai Aguilera <gkalgan@gmail.com>
2012-02-10 15:05:35 +0000

    * Neighbor list is initialized to emtpy

--- experiments/measures/detection/AvgNeighbors.py
+++ experiments/measures/detection/AvgNeighbors.py
@@ -9,19 +9,25 @@ class AvgNeighbors(GenericAvgMeasure):
 	def __init__(self, period, simulationTime):
 		GenericAvgMeasure.__init__(self, period, simulationTime, Units.NEIGHBORS, maintainLastValue=True)
 		
+		self.__initializePattern = re.compile('INFO  peer.BasicPeer  - Peer ([0-9]+) initializing.*?')
 		self.__appearPattern = re.compile('DEBUG detection.beaconDetector.BeaconDetector  - Peer ([0-9]+) has new neighbors: (\[.*?\]) ([0-9]+\,[0-9]+).*?')
 		self.__disappearPattern = re.compile('DEBUG detection.beaconDetector.BeaconDetector  - Peer ([0-9]+) has lost neighbors: (\[.*?\]) ([0-9]+\,[0-9]+).*?')
 		
 		self.__currentNeighbors = {}
 
 	def parseLine(self, line):
+		m = self.__initializePattern.match(line)
+		if m is not None:
+			peer = m.group(1)
+			self.__currentNeighbors[peer] = []
+
+			return
+					
 		m = self.__appearPattern.match(line)
 		if m is not None:
 			peer = m.group(1)
 			neighbors = eval(m.group(2))
 			time = float(m.group(3).replace(',','.'))
-			
-			self.__checkPeer(peer)
 			
 			self.__currentNeighbors[peer] += neighbors
 			
@@ -35,16 +41,10 @@ class AvgNeighbors(GenericAvgMeasure):
 			neighbors = eval(m.group(2))
 			time = float(m.group(3).replace(',','.'))
 			
-			self.__checkPeer(peer)
-			
 			for neighbor in neighbors:
 				self.__currentNeighbors[peer].remove(neighbor)
 				
 			self.periodicAvgValues.addValue(len(self.__currentNeighbors[peer]), time)
-			
-	def __checkPeer(self, peer):
-		if not peer in self.__currentNeighbors:
-			self.__currentNeighbors[peer] = [] 
 			
 	def isDiscardable(self):
 		return False
