2c4c1222e77ea11ff187bae82f15a5ecf3c02324
Unai Aguilera <gkalgan@gmail.com>
2012-02-12 10:21:48 +0000

    * New measure for traffic overhead added.

--- experiments/measures/generic/Units.py
+++ experiments/measures/generic/Units.py
@@ -9,6 +9,7 @@ COMPOSITIONS = 'compositions'
 SEARCHES = 'searches'
 HOPS = 'hops'
 MESSAGE_OVERHEAD = 'messages/s node'
+MESSAGE_TRAFFIC_OVERHEAD = 'KB/s node'
 
 def str_formatter(units, value):
     if units == MILLIS:
---
+++ experiments/measures/multicast/MulticastTrafficOverhead.py
@@ -0,0 +1,56 @@
+import re
+from measures.periodicValues.PeriodicValues import PeriodicValues
+from measures.generic.GenericMeasure import GenericMeasure as GenericMeasure
+import measures.generic.Units as Units
+
+class MulticastTrafficOverhead(GenericMeasure):
+	def __init__(self, period, simulationTime):		
+		GenericMeasure.__init__(self, '', period, simulationTime, Units.MESSAGE_TRAFFIC_OVERHEAD)
+		
+		measures = ('dissemination.newProtocol.message.TableMessage', 
+				   'multicast.search.message.SearchMessage', 
+				   'multicast.search.message.SearchResponseMessage',
+				   'multicast.search.message.RemoveRouteMessage',
+				   'multicast.search.message.RemoveParametersMessage')
+		
+		self.__sizePatterns = []
+		
+		self.__initializePattern = re.compile('INFO  peer.BasicPeer  - Peer ([0-9]+) initializing ([0-9]+\,[0-9]+).*?')
+		
+		for measure in measures:
+			pattern = "DEBUG .*?  - Peer .*? sending " + measure + " .*? ([0-9]+) bytes ([0-9]+\,[0-9]+).*?"
+			self.__sizePatterns.append(re.compile(pattern))
+		
+		self.__totalSize = 0
+		
+		self.__neighbors = 0
+		
+	def parseLine(self, line):
+		m = self.__initializePattern.match(line)
+		if m is not None:
+			peer = m.group(1)
+			time = float(m.group(2).replace(',','.'))
+			
+			self.__neighbors += 1
+
+			return
+		
+		for sizePattern in self.__sizePatterns:
+			m = sizePattern.match(line)
+			if m is not None:
+				size = int(m.group(1))
+				time = float(m.group(2).replace(',','.'))
+				
+				self.__totalSize += size
+	
+				return
+			
+	def getValues(self): 
+		return PeriodicValues(0, self.getPeriod(), self.getSimulationTime())
+
+	def getTotalValue(self):
+		print self.__totalSize
+		return self.__totalSize / float(self.__neighbors) / self.getSimulationTime() / 1024.0
+		
+		
+		
