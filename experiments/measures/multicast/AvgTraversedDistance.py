35959ecd7193690aaad83754758fd5d1c26f3e2d
Unai Aguilera <gkalgan@gmail.com>
2012-02-19 17:02:21 +0000

    * Traversed distance measure fixed.

--- experiments/measures/multicast/AvgTraversedDistance.py
+++ experiments/measures/multicast/AvgTraversedDistance.py
@@ -10,8 +10,8 @@ class AvgTraversedDistance(GenericAvgMeasure):
 	def __init__(self, period, simulationTime):		
 		GenericAvgMeasure.__init__(self, period, simulationTime, Units.HOPS)
 		
-		self.__acceptedPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer [0-9]+ accepted multicast.search.message.SearchMessage .*? distance ([0-9]+) ([0-9]+\,[0-9]+).*?')
-		self.__foundPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? found parameters .*? in node [0-9]+ searchID .*? distance ([0-9]+) .*? ([0-9]+\,[0-9]+).*?')			
+		self.__acceptedPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer [0-9]+ accepted multicast.search.message.SearchMessage .*? distance ([0-9]+) .*? ([0-9]+\,[0-9]+).*?')
+		self.__foundPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? found parameters .*? in node [0-9]+ searchID .*? distance ([0-9]+) ([0-9]+\,[0-9]+).*?')			
 	
 	def parseLine(self, line):
 		m = self.__acceptedPattern.match(line)
