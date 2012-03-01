6783965ac93e031634c5f9f6400333b8191aa870
Unai Aguilera <gkalgan@gmail.com>
2012-03-01 21:20:54 +0000

    * Invalid measure removed.

--- experiments/measures/graphcreation/SentCollisionMessages.py
+++
@@ -1,8 +0,0 @@
-from measures.generic.SentPayloadXXXMessages import SentPayloadXXXMessages
-
-class SentCollisionMessages(SentPayloadXXXMessages):
-	"""Total number of sent collision messages"""
-	
-	def __init__(self, period, simulationTime):
-		SentPayloadXXXMessages.__init__(self, 'graphcreation.collisionbased.message.CollisionMessage', period, simulationTime)
-		
