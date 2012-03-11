b813bc0f5f3b37c214cea46e5a602858134a1d78
Unai Aguilera <gkalgan@gmail.com>
2012-03-11 10:27:13 +0000

    * Forwarded messages measure was incorrectly implemented.

--- experiments/measures/graphcreation/ForwardedMessages.py
+++ experiments/measures/graphcreation/ForwardedMessages.py
@@ -6,7 +6,7 @@ class ForwardedMessages(GenericMeasure):
 	def __init__(self, messageClass, period, simulationTime):
 		self.__messageClass = messageClass
 		
-		GenericMeasure.__init__(self, r"DEBUG .*?  - Peer .*? forwarding " + self.__messageClass + " ([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
+		GenericMeasure.__init__(self, r"DEBUG .*?  - Peer .*? forwarding " + self.__messageClass + " .*? ([0-9]+\,[0-9]+).*?", period, simulationTime, Units.MESSAGES)
 			
 	def parseLine(self, line):
 		self.parseInc(line)
