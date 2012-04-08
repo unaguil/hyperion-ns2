08d8ec36b4ad676be5516aa80a880048397bf964
Unai Aguilera <gkalgan@gmail.com>
2012-04-08 12:39:54 +0000

    * Unused code removed.  * Variable names changed.

--- experiments/measures/multicast/AvgSearchTimeFirstOcurrence.py
+++ experiments/measures/multicast/AvgSearchTimeFirstOcurrence.py
@@ -7,8 +7,8 @@ class AvgSearchTimeFirstOcurrence(GenericAvgMeasure):
 	def __init__(self, period, simulationTime):		
 		GenericAvgMeasure.__init__(self, period, simulationTime, Units.SECONDS)
 		
-		self.__searchPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? started search for parameters (\[.*?\]) searchID (\(.*?\)) .*? ([0-9]+\,[0-9]+).*?')
-		self.__foundPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? found parameters (\[.*?\]) in node ([0-9]+) searchID (\(.*?\)) .*? ([0-9]+\,[0-9]+).*?')							 
+		self.__startPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? started search for parameters (\[.*?\]) searchID (\(.*?\)) .*? ([0-9]+\,[0-9]+).*?')
+		self.__endPattern = re.compile('DEBUG multicast.search.ParameterSearchImpl  - Peer .*? found parameters (\[.*?\]) in node ([0-9]+) searchID (\(.*?\)) .*? ([0-9]+\,[0-9]+).*?')							 
 		
 		self.__currentSearches = {}
 	
@@ -16,14 +16,14 @@ class AvgSearchTimeFirstOcurrence(GenericAvgMeasure):
 		return [str.strip() for str in str[1:-1].split(',')]
 
 	def parseLine(self, line):
-		m = self.__searchPattern.match(line)
+		m = self.__startPattern.match(line)
 		if m is not None:
 			searchID = m.group(2)
 			time = float(m.group(3).replace(',','.'))	
 			self.__currentSearches[searchID] = time
 			return
 		
-		m = self.__foundPattern.match(line)
+		m = self.__endPattern.match(line)
 		if m is not None:
 			searchID = m.group(3)
 			time = float(m.group(4).replace(',','.')) 
@@ -32,9 +32,4 @@ class AvgSearchTimeFirstOcurrence(GenericAvgMeasure):
 				self.periodicAvgValues.addValue(time - self.__currentSearches[searchID], time)
 				del self.__currentSearches[searchID]
 			
-			return 
-								
-	def __checkParameter(self, parameter, list):
-		if not parameter in list:
-			list[parameter] = 0 
-	
+			return 	
