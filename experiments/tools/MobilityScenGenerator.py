ebc1121e82df4954887a6ec28b98e12e94b1d602
Unai Aguilera <gkalgan@gmail.com>
2012-04-14 13:21:11 +0000

    * New scenarios added.

--- experiments/tools/MobilityScenGenerator.py
+++ experiments/tools/MobilityScenGenerator.py
@@ -67,8 +67,8 @@ def generateNS2MobilityScenario(nodes, finishTime, gridW, gridH, minSpeed, maxSp
 def main():    
     nodes = 50
     finishTime = 200
-    gridW = 500
-    gridH = 500
+    gridW = 400
+    gridH = 400
 
     minSpeed = 0.00001
     maxSpeed = 5.0
@@ -77,7 +77,7 @@ def main():
 
     transmissionRange = 100.0
    
-    for pauseTime in range(50, 1200, 1200): 
+    for pauseTime in range(100, 1200, 1200): 
         generateNS2MobilityScenario(nodes, finishTime, gridW, gridH, minSpeed, maxSpeed, pauseTime, transmissionRange, numScenarios)
 
 if __name__ == '__main__':
