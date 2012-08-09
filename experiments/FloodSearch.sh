414b032f701d5ed38eb8d2b3fb9b4e4a6868ee91
Unai Aguilera <gkalgan@gmail.com>
2012-08-09 13:46:05 +0000

    * Unused experiment launchers removed.

--- experiments/FloodSearch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python Experiment.py -c configurations/comparison -i FloodedSearchInterval.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FloodedSearchCL.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FloodedSearchPause.xml -d True -o ~/ -w ~/
--- experiments/experiments-multicast-high-extra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#python Experiment.py -c ~/experiments/multicast/high/static -i NonRepeatServicesFixedScenario.xml -d True -w ~/static -o ~/static
-python Experiment.py -c ~/experiments/multicast/high/100 -i SimultaneousSearches.xml -d True -w ~/100 -o ~/100
-python Experiment.py -c ~/experiments/multicast/high/50 -i SimultaneousSearches.xml -d True -w ~/50 -o ~/50
