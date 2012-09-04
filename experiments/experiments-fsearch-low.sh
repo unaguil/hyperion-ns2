54e42c6264dfbe229ed28b45f60773b83a72e43c
Unai Aguilera <gkalgan@gmail.com>
2012-09-04 14:33:36 +0000

    * Old launchers removed.

--- experiments/experiments-fsearch-low.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-python Experiment.py -c ~/experiments/fsearch/low/$1 -i Replication.xml -d True -w ~/ -o ~/
-python Experiment.py -c ~/experiments/fsearch/low/$1 -i CompositionLength.xml -d True -w ~/ -o ~/
-python Experiment.py -c ~/experiments/fsearch/low/$1 -i Distribution.xml -d True -w ~/ -o ~/
-python Experiment.py -c ~/experiments/fsearch/low/$1 -i SearchFreq.xml -d True -w ~/ -o ~/
-
