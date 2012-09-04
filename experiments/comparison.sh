b130de51de06561f7cef22f22634c68d5b7fd53e
Unai Aguilera <gkalgan@gmail.com>
2012-09-04 14:44:18 +0000

    * Old launcher removed.

--- experiments/comparison.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python Experiment.py -c configurations/comparison -i FloodedSearchPause.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FGraphSearchPause.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FloodedSearchCL.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FGraphSearchCL.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FloodedSearchCW.xml -d True -o ~/ -w ~/
-python Experiment.py -c configurations/comparison -i FGraphSearchCW.xml -d True -o ~/ -w ~/
